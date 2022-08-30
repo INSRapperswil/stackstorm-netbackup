b2b78d86f8e9372eaca0433a36ab853b4a0022e5
Marco Martinez <mmmartinez@bluewin.ch>
2022-08-30 15:57:29 +0200

    add variable message

--- actions/send_mail.py
+++ actions/send_mail.py
@@ -4,13 +4,13 @@ from lib.actions import BaseAction
 
 
 class SendMail(BaseAction):
-    def run(self, recipient_address, mail_server, mail_sender_address):
+    def run(self, recipient_address, mail_server, mail_sender_address, mail_message):
 
         msg = EmailMessage()
         msg['From'] = mail_sender_address
         msg['To'] = recipient_address
         msg['Subject'] = "Network device backup failed!"
-        msg.set_content("Something went wrong with the backup! Check Stackstorm")
+        msg.set_content(mail_message)
 
         try:
             with smtplib.SMTP(mail_server) as server_instance:
