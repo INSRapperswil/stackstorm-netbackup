3e16abb02944da5a61cac2163682f0d688031416
Marco Martinez <mmmartinez@bluewin.ch>
2022-08-30 14:59:36 +0200

    add alerting

---
+++ actions/send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.message import EmailMessage
+from lib.actions import BaseAction
+
+
+class SendMail(BaseAction):
+    def run(self, recipient_address, mail_server, mail_sender_address):
+
+        msg = EmailMessage()
+        msg['From'] = mail_sender_address
+        msg['To'] = recipient_address
+        msg['Subject'] = "Network device backup failed!"
+        msg.set_content("Something went wrong with the backup! Check Stackstorm")
+
+        try:
+            with smtplib.SMTP(mail_server) as server_instance:
+                server_instance.send_message(msg)
+        except Exception as e:
+            self.logger.error(e)
+            return False, ""
+
+        return True, "Mail transmitted successfully!"
